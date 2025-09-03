7ff30704ab494536194aa502c2bac34cd9bf4428
phani <1308phanidattakandukuri@gmail.com>
2025-09-03 21:28:28 +0530

    Fix vercel.json and schema import paths for deployment

--- schemaTypes/index.js
+++ schemaTypes/index.js
@@ -1,1 +1,6 @@
-export const schemaTypes = []
+import blog from '../../sanity/schemas/blog'
+import author from '../../sanity/schemas/author'
+import blockContent from '../../sanity/schemas/blockContent'
+import code from '../../sanity/schemas/code'
+
+export const schemaTypes = [blog, author, blockContent, code]
